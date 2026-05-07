e8282dbd49eaea59e0ce62b0b931fff1a71825d8
Alex Prochazka <alexprosd@gmail.com>
2026-05-06 20:27:23 -0700

    add weapon prefabs and materials and change WeaponManager to instantiate from prefabs

--- Assets/Scripts/Weapons/WeaponManager.cs
+++ Assets/Scripts/Weapons/WeaponManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
+    [SerializeField] private WeaponBase[] weaponPrefabs;
+
     public WeaponBase[]  weapons      { get; private set; }
     public int           currentIndex { get; private set; }
     public WeaponBase    currentWeapon => weapons[currentIndex];
@@ -9,15 +11,6 @@ public class WeaponManager : MonoBehaviour
     public event System.Action<int> OnWeaponChanged;
 
     private Camera _camera;
-
-    // Viewmodel: local position and scale relative to the camera for each weapon slot.
-    private static readonly (Vector3 pos, Vector3 scale)[] ViewmodelConfig =
-    {
-        (new Vector3( 0.25f, -0.22f, 0.50f), new Vector3(0.05f, 0.05f, 0.44f)),  // Rifle
-        (new Vector3( 0.22f, -0.20f, 0.42f), new Vector3(0.09f, 0.07f, 0.30f)),  // Shotgun
-        (new Vector3( 0.25f, -0.22f, 0.50f), new Vector3(0.06f, 0.06f, 0.50f)),  // Machine Gun
-        (new Vector3( 0.22f, -0.19f, 0.55f), new Vector3(0.03f, 0.03f, 0.60f)),  // Railgun
-    };
 
     // Build weapons in Awake so PlayerHUD can safely read them in Start.
     void Awake()
@@ -57,35 +50,12 @@ public class WeaponManager : MonoBehaviour
 
     void BuildWeapons()
     {
-        weapons    = new WeaponBase[4];
-        weapons[0] = BuildWeapon<Rifle>(0);
-        weapons[1] = BuildWeapon<Shotgun>(1);
-        weapons[2] = BuildWeapon<MachineGun>(2);
-        weapons[3] = BuildWeapon<Railgun>(3);
-    }
-
-    T BuildWeapon<T>(int index) where T : WeaponBase
-    {
-        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        go.name = typeof(T).Name + "_Viewmodel";
-        go.transform.SetParent(_camera.transform, false);
-        go.transform.localPosition = ViewmodelConfig[index].pos;
-        go.transform.localScale    = ViewmodelConfig[index].scale;
-
-        Destroy(go.GetComponent<Collider>());
-
-        // AddComponent triggers Awake, which sets hudColor on the weapon.
-        T weapon = go.AddComponent<T>();
-
-        Renderer rend   = go.GetComponent<Renderer>();
-        Shader   shader = Shader.Find("Universal Render Pipeline/Lit") ?? Shader.Find("Standard");
-        Material mat    = new Material(shader);
-        mat.SetColor("_BaseColor", weapon.hudColor);
-        mat.SetColor("_Color",     weapon.hudColor);
-        rend.material = mat;
-
-        go.SetActive(false);
-        return weapon;
+        weapons = new WeaponBase[weaponPrefabs.Length];
+        for (int i = 0; i < weaponPrefabs.Length; i++)
+        {
+            weapons[i] = Instantiate(weaponPrefabs[i], _camera.transform, false);
+            weapons[i].gameObject.SetActive(false);
+        }
     }
 
     public void EquipWeapon(int index)
